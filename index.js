1ac06c1b2253ba9d31e79164e41c3f3550ffc633
Theodor Johansson <t.j.u@telia.com>
2023-10-24 11:29:21 +0200

    Fixed date conversions

--- index.js
+++ index.js
@@ -20,8 +20,45 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
+/* app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
+}); */
+
+// This is the solution for this task
+app.get("/api/:date?", function (req, res) {
+  
+  receivedDate = req.params.date
+  console.log(receivedDate)
+
+  var isDate = function(date) {
+    return (new Date(date) !== "Invalid Date") && !isNaN(new Date(date));
+  }
+  if (receivedDate == undefined) {
+    let emptyDate = new Date()
+    res.json({"unix": emptyDate, "utc": emptyDate.toString()})
+  }
+  else if (!isDate(receivedDate)) {
+    res.json({ error : "Invalid Date" })
+  }
+  else {
+    let date = new Date(receivedDate)
+    res.json({"unix": date, "utc": date.toString()})
+  }
+
+  /* if (req.params.date == '') {
+    date = new Date()
+  }
+  else {
+    date = Number(req.params.date)
+  }
+  console.log(date)
+  console.log(new Date())
+  console.log(Date.now())
+  console.log(new Date(0))
+  const dateEpochTime = new Date(date); // passing epoch timestamp
+  console.log(dateEpochTime)
+  res.json({"unix": date, "utc": dateEpochTime.toString()}) */
+  
 });
 
 
